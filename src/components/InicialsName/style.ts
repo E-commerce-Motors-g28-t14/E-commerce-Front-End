25475435c7ab39a531b69d3d0c12410a76ca46db
annekarolle <102996739+annekarolle@users.noreply.github.com>
2023-06-05 21:47:33 -0300

    fix/ fiz alterações na composição do componente username, dividindo em dois componentes devido a quantidade de utilização na aplicação.

---
+++ src/components/InicialsName/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ContainerInitial = styled.div`
+
+    border-radius: 50%;  
+    left: 277px;
+    top: 23.5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;    
+
+
+h2{
+    font-family: Inter; 
+    font-weight: 700;
+    letter-spacing: 0em;
+    text-align: left;
+    color: var(--color-white-fixed);
+}
+`
