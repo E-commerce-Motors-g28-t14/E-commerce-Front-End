c2ac2e51dbd649425dd4f80de02333ee3c514c94
srod-douglas <srod.douglas@gmail.com>
2023-06-07 02:21:04 -0300

    feat: create comment card detail page car

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { CardAdvertiser } from './components/CardAdvertiser';
 import CommentArea from "./components/CommentArea/CommentArea";
+import { CommentCard } from './components/CommentCard';
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import PhotoGalery from "./components/PhotoGalery/PhotoGalery";
@@ -8,8 +9,9 @@ const App = () => {
   return (
     <>
       <Header />
-      <CommentArea/>
+      <CommentArea />
       <PhotoGalery />
+      <CommentCard />
       <CardAdvertiser/>
       <Footer/>
     </>
