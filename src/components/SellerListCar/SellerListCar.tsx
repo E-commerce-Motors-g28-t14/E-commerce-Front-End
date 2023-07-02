3547327291b84c2473a89d12f07604e1e4d6b0df
annekarolle <102996739+annekarolle@users.noreply.github.com>
2023-07-02 13:46:31 -0300

    Style - A rendering condition has been added to the gerenralsselerpage / seller page / home page, which checks for the existence of the car list while the loading component is being displayed

--- src/components/SellerListCar/SellerListCar.tsx
+++ src/components/SellerListCar/SellerListCar.tsx
@@ -6,6 +6,7 @@ import { StyledButton } from "../../styles/buttons";
 import { ModalBody } from "../ModalBody";
 import { FormUpdateCar } from "../FormUpdateCar";
 import { useModalHook } from "../../hooks";
+import { Loading } from "../Loading/Looading";
 
 const SellerListCar = () => {
   const { user } = useContext(UserContext);
@@ -28,66 +29,72 @@ const SellerListCar = () => {
 
   return (
     <ListCarContainer>
-      {ListCarUser && ListCarUser.length > 0 ? (
-        ListCarUser.map((car, key) => {         
-          const photoCape = car.photos && car.photos.find((photo) => photo.isCover === true);
-          
-          return (
-            <ListCar key={key}>
-              <div>
-                <img
-                  src={photoCape?.imageLink?.toString()}
-                  alt={`Foto do carro ${car.model}`}
-                />
-              </div>
-              <div>
-                <span>{car.brand}</span>
-                <span>-</span>
-                <span>{car.model}</span>
-              </div>
-              <div>
-                <p>{car.description}</p>
-              </div>
-              <div className="info-car">
-                <div className="info-car-tags">
-                  <span className="tag-car-info">{car.km} KM</span>
-                  <span className="tag-car-info">{car.year}</span>
+      {ListCarUser ? (
+        ListCarUser.length > 0 ? (
+          ListCarUser.map((car, key) => {
+            const photoCape =
+              car.photos && car.photos.find((photo) => photo.isCover === true);
+  
+            return (
+              <ListCar key={key}>
+                <div>
+                  <img
+                    src={photoCape?.imageLink?.toString()}
+                    alt={`Foto do carro ${car.model}`}
+                  />
                 </div>
-
-                <span className="tag-car-info price">
-                  R$ {parseInt(car.price).toFixed(2).replace(".", ",")}
-                </span>
-
-                <div
-                  className="isActive"
-                  style={{
-                    backgroundColor: car.isActive
-                      ? "var(--color-brand-1)"
-                      : "var(--color-grey-4)",
-                  }}
-                >
-                  <span>{car.isActive ? "Ativo" : "Inativo"}</span>
+                <div>
+                  <span>{car.brand}</span>
+                  <span>-</span>
+                  <span>{car.model}</span>
                 </div>
-              </div>
-              <div className="btn-update-ad">
-                <StyledButton
-                  className="medium outline1"
-                  onClick={() => handleClick(car.id)}
-                >
-                  Editar
-                </StyledButton>
-                <StyledButton className="medium outline1">
-                  Ver detalhes
-                </StyledButton>
-              </div>
-            </ListCar>
-          );
-        })
+                <div>
+                  <p>{car.description}</p>
+                </div>
+                <div className="info-car">
+                  <div className="info-car-tags">
+                    <span className="tag-car-info">{car.km} KM</span>
+                    <span className="tag-car-info">{car.year}</span>
+                  </div>
+  
+                  <span className="tag-car-info price">
+                    R$ {parseInt(car.price).toFixed(2).replace(".", ",")}
+                  </span>
+  
+                  <div
+                    className="isActive"
+                    style={{
+                      backgroundColor: car.isActive
+                        ? "var(--color-brand-1)"
+                        : "var(--color-grey-4)",
+                    }}
+                  >
+                    <span>{car.isActive ? "Ativo" : "Inativo"}</span>
+                  </div>
+                </div>
+                <div className="btn-update-ad">
+                  <StyledButton
+                    className="medium outline1"
+                    onClick={() => handleClick(car.id)}
+                  >
+                    Editar
+                  </StyledButton>
+                  <StyledButton className="medium outline1">
+                    Ver detalhes
+                  </StyledButton>
+                </div>
+              </ListCar>
+            );
+          })
+        ) : (
+          <div className="info-message">
+            <h4>Você não possui nenhum anúncio.</h4>
+          </div>
+        )
       ) : (
-        <div className="info-message"> <h4>Você não possui nenhum anúncio.</h4> </div>
-      
+        <Loading />
       )}
-      {/* <>
+      <>
         {isOpenModal ? (
           <ModalBody close={toggleModal}>
             <FormUpdateCar
@@ -112,7 +119,7 @@ const SellerListCar = () => {
             />
           </ModalBody>
         ) : null}
-      </> */}
+      </>
     </ListCarContainer>
   );
 };
